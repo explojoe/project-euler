18503fd7bbd94298a2dbec0b48c3aee43613b745
Joshua Wentzel <explojoe@yahoo.com>
2019-03-25 15:07:04 -0700

    Finished problem 10.

--- src/10.cpp
+++ src/10.cpp
@@ -14,54 +14,60 @@ bool isPrime(uint32_t num) {
     return true;
 }
 
-uint32_t solveProblem(uint32_t num) {
-    uint32_t sumPrimes = 2;
+// for debugging
+void printSeive(std::vector<bool> seive, uint32_t num) {
+    for (uint32_t i = 0; i < num; i++) {
+        std::cout << "seive[" + std::to_string(i) +
+                         "] = " + std::to_string(seive[i]) + "\n";
+    }
+}
 
-    std::vector<bool> seive(num);
+uint64_t solveProblem(uint32_t num) {
+    uint64_t sumPrimes = 2;
+
+    std::vector<bool> seive(num); // I am using the seive of Eratosthenes
 
     // set all elements to false
     for (uint32_t i = 0; i < num; i++) {
         seive[num] = false;
     }
-    std::cout << "2 is prime.\n";
+
+    // mark all multiples of 2
     for (uint32_t i = 2; i < num; i += 2) {
         if (i % 2 == 0) {
             seive[num - 1] = true;
         }
     }
+
+    // we don't need to mark any multiples of numbers over sqrt(n)
+    uint32_t rootNum = sqrt(num);
+    if (rootNum % 2 == 0) {
+        rootNum--;
+    }
+
     // for all numbers between 2 and sqrt(num)
-    for (uint32_t i = 3; i < sqrt(num); i += 2) {
+    for (uint32_t i = 3; i <= rootNum; i += 2) {
 
         if (!seive[i - 1]) {
             sumPrimes += i;
-            std::cout << std::to_string(i) + " is prime.\n";
-
             for (uint32_t a = i; a < num; a += i) {
                 seive[a - 1] = true;
             }
         }
     }
-    for (uint32_t i = sqrt(num); i < num; i += 2) {
+    // printSeive(seive, num);
+
+    // all unmarked numbers above sqrt(n) are guaranteed to be prime
+    for (uint32_t i = rootNum; i <= num; i += 2) {
         if (!seive[i - 1]) {
             sumPrimes += i;
-            std::cout << std::to_string(i) + " IS prime.\n";
-            for (uint32_t a = i; a < num; a += i) {
-                // seive[a - 1] = true;
-            }
         }
     }
 
     return sumPrimes;
-    // uint32_t sumPrimes = 1;
-    // for (uint32_t i = 3; i < num; i += 2) {
-    // if (isPrime(i)) {
-    //  sumPrimes += i;
-    //}
-    //}
-    // return sumPrimes;
 }
 
 int main(int argc, char **argv) {
-    doMain(solveProblem);
+    doMain<uint32_t, uint64_t>(solveProblem);
     return 0;
 }
